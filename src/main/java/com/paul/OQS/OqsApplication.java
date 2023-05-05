cbd49137c8d0de8fd0c1305acfc7cf5a475677ca
paul webo <paulwebo38@gmail.com>
2023-05-05 21:32:18 +0300

    configured the h2 database

---
+++ src/main/java/com/paul/OQS/OqsApplication.java
@@ -0,0 +1,13 @@
+package com.paul.OQS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paul/OQS/OqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.OQS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
